cd10d05d4efe04531caa92f963393e40bca105df
Gustavo Mendes de Oliveira <167227958+gustavomoliveira@users.noreply.github.com>
2024-10-17 20:47:15 +0200

    refactor: finished two more functions
    def multiplicar() and def subtrair()

--- semana01/calculadora_refact.py
+++ semana01/calculadora_refact.py
@@ -13,32 +13,38 @@ def numero_2():
     return num_2
 
 def somar(num_1, num_2):
-    print(f'A soma entre {num_1} e {num_2} é {num_1 + num_2}')
+    resultado = num_1 + num_2
+    print(f'A soma entre {num_1} e {num_2} é {resultado}')
 
 def subtrair(num_1, num_2):
-    print(f'A subtração entre {num_1} e {num_2} é {num_1 - num_2}')
+    resultado = num_1 - num_2
+    print(f'A subtração entre {num_1} e {num_2} é {resultado}')
 
 def multiplicar(num_1, num_2):
-    print(f'A multiplicação entre {num_1} e {num_2} é {num_1 * num_2}')
+    resultado = num_1 * num_2
+    print(f'A multiplicação entre {num_1} e {num_2} é {resultado}')
 
-def subtrair(num_1, num_2):
-        if num_2 == 0:
-            print('Erro: divisão por 0.')
-        else:
-            print(f'A divisão entre {num_1} e {num_2} é {num_1 / num_2}')
+def dividir(num_1, num_2):
+    if num_2 == 0:
+        print('Erro: divisão por 0.')
+    else:
+        resultado = num_1 / num_2
+        print(f'A divisão entre {num_1} e {num_2} é {resultado}')
+
+def executar_operacao(operacao, num_1, num_2):
+    match operacao:
+        case '+':
+            somar(num_1, num_2)
+        case '-':
+            subtrair(num_1, num_2)
+        case '*':
+            multiplicar(num_1, num_2)
+        case '/':
+            dividir(num_1, num_2)
+        case _:
+            print('Digite um operador válido.')
 
 operacao = menu()
 num_1 = numero_1()
 num_2 = numero_2()
-
-match operacao:
-    case '+':
-        somar(num_1, num_2)
-    case '-':
-        subtrair(num_1, num_2)
-    case '*':
-        multiplicar(num_1, num_2)
-    case '/':
-        subtrair(num_1, num_2)
-    case _:
-        print('Digite um operador válido.')
+executar_operacao(operacao, num_1, num_2)
