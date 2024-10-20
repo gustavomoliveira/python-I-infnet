7323ea4771b4a46d5326b3181a0063c47de7fef7
Gustavo Mendes de Oliveira <167227958+gustavomoliveira@users.noreply.github.com>
2024-10-20 09:36:49 +0200

    refactor: changed the match case structure

--- semana02/validacao_data_refact.py
+++ semana02/validacao_data_refact.py
@@ -1,27 +1,28 @@
 def validar_data(dia, mes, ano):
-    if mes == 4 or mes == 6 or mes == 9 or mes == 11:
-        if dia >= 1 and dia <= 30:
-            print('Data válida.')
-        else:
-            print('Dia inválido.')
-    elif mes == 1 or mes == 3 or mes == 5 or mes == 7 or mes == 8 or mes == 10 or mes == 12:
-        if dia >= 1 and dia <= 31:
-            print('Data válida.')
-        else:
-            print('Dia inválido.')
-    elif mes == 2:
-        if ano % 4 == 0:
-            if dia >= 1 and dia <= 29:
+    match mes:
+        case 4 | 6 | 9 | 11:
+            if dia >= 1 and dia <= 30:
                 print('Data válida.')
             else:
                 print('Dia inválido.')
-        else:
-            if dia >= 1 and dia <= 28:
+        case 1 | 3 | 5 | 7 | 8 | 10 | 12:
+            if dia >= 1 and dia <= 31:
                 print('Data válida.')
             else:
                 print('Dia inválido.')
-    else:
-        print('Mês inválido.')
+        case 2:
+            if ano % 4 == 0:
+                if dia >= 1 and dia <= 29:
+                    print('Data válida.')
+                else:
+                    print('Dia inválido.')
+            else:
+                if dia >= 1 and dia <= 28:
+                    print('Data válida.')
+                else:
+                    print('Dia inválido.')
+        case _:
+            print('Mês inválido.')
 
 # princípio do SRP(single responsability principle)
 def entrar_data():
